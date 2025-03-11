3ca93b98f0d234331363063c500e8a0aad304878
wuttichai-jeenkaew <wuttichai.jeenkaew@gmail.com>
2025-03-11 14:54:12 +0700

    feat:creat navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+
+import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0);
+  
 
   return (
     <>
-      <section>
-        <h1>My First React App on Vite</h1>
-      </section>
+        <Navbar/>    
+  
     </>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,32 @@
+
+
+
+function Navbar() {
+  return (
+    <>
+      <nav className="flex items-center justify-between py-4 px-8 bg-Brown-100 border-b relative">
+      <a className="text-2xl font-bold">
+        koRN Blog <span className="text-green-500">.</span>
+      </a>
+      <div className="hidden md:flex space-x-4">
+          <button
+           
+            className="px-9 py-2 rounded-full border"
+          >
+            Log in
+          </button>
+          <button
+           
+            className="px-8 py-2 bg-gray-900 text-white rounded-full hover:bg-gray-700 transition-colors"
+          >
+            Sign up
+          </button>
+        </div>
+      </nav>
+
+
+    </>
+  );
+}
+
+export default Navbar;
