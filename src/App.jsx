2ff9ddcb799cf608f3c12bbff38f679e7f5ab597
wuttichai-jeenkaew <wuttichai.jeenkaew@gmail.com>
2025-03-12 09:28:14 +0700

    fix:name components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 import './App.css'
 import Navbar from './components/Navbar'
-import LadingSection from './components/LadingSection'
+import HeroSection from './components/HeroSection'
 
 function App() {
   
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
         <Navbar/> 
-        <LadingSection/>
+        <HeroSection/>
   
     </>
   )
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,12 +1,12 @@
-function LadingSection() {
+function HeroSection() {
     return (
       <main className="container px-4 py-8 lg:py-16 mx-auto">
         <div className="flex flex-col lg:flex-row items-center">
           <div className="lg:w-1/3 mb-8 lg:mb-0 lg:pr-8">
-            <h1 className="text-4xl lg:text-5xl font-bold mb-4">
+            <h1 className="text-4xl lg:text-5xl font-bold mb-4" text-center>
               Stay <br className="hidden lg:block" />
               Informed, <br />
-              Stay Inspired,
+              Stay Inspired
             </h1>
             <p className="text-lg text-gray-500">
               Discover a World of Knowledge at Your Fingertips. Your Daily Dose of
@@ -36,4 +36,4 @@ function LadingSection() {
     );
   }
 
-  export default LadingSection
+  export default HeroSection
