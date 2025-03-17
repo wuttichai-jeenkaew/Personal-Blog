7fe9eadc83bc2f13f87789730fc58a95f0a95ec3
wuttichai-jeenkaew <wuttichai.jeenkaew@gmail.com>
2025-03-17 22:45:06 +0700

    fix: HeroSection

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,7 +1,7 @@
 function HeroSection() {
     return (
-      <main className="container px-4 py-8 lg:py-16 mx-auto">
-        <div className="flex flex-col lg:flex-row items-center">
+      <main className="container px-4 py-8 lg:py-16 mx-auto ">
+        <div className="flex flex-col lg:flex-row items-center justify-center">
           <div className="lg:w-1/3 mb-8 lg:mb-0 lg:pr-8" >
             <h1 className="text-4xl lg:text-5xl font-bold mb-4 text-center lg:text-end">
               Stay <br className="hidden lg:block" />
@@ -13,11 +13,13 @@ function HeroSection() {
               Inspiration and Information.
             </p>
           </div>
+          <div className="w-[386px] ">
           <img
             src="https://s3-alpha-sig.figma.com/img/14d0/ff1e/c045ed1d618b25c84aa4327331ecdaaf?Expires=1743379200&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=JHQ2w3412HO781E-WpCXE8ZvxiBZ1-bU7AMS6uX1rYQxj36cHvat46R6zecHKjy6skL4NpNbBGph7hwEVKdCLK~3atoUDPhuDYCy0DjcFSNqW9pzCWHLGpwD-Cdm2R8WJwzkH9EXUJFUqJwBcLuINtfRFfB26CpDUsnVPf3Sns9-mUaDY-nJmS5pWlpES5~0CYbDWXQtIzFtPHNojdqjwqYW3WZIBdLD3vJHviJwphXnnnYVK7--WnGqf58nZfUi7lHEi-7SfLr9FXppVQ0cRSLRu5xNq2KmnSfLxIK7GAANZsnZxkahHsjy8s-RH6VUn8FjtjthHA5E58FIGd3nLA__"
             alt="UserProfile"
-            className="h-[529px] w-[386px] object-cover rounded-lg shadow-lg lg:w-1/3 mx-4 mb-8 lg:mb-0"
+            className="h-[529px] w-[386px] object-cover rounded-lg shadow-lg mb-8 lg:mb-0 "
           />
+          </div>
           <div className="lg:w-1/3 lg:pl-8">
             <h2 className="text-xl font-semibold mb-2">-Author</h2>
             <h3 className="text-2xl font-bold mb-4">Thompson P.</h3>
