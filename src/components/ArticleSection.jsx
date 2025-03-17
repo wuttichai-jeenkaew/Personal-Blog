d0d88ed52da3c7a473dab0880a1ccdadcc05384d
wuttichai-jeenkaew <wuttichai.jeenkaew@gmail.com>
2025-03-17 17:37:57 +0700

    fix:blogcard

--- src/components/ArticleSection.jsx
+++ src/components/ArticleSection.jsx
@@ -11,7 +11,7 @@ import {
 } from "@/components/ui/select";
 import ArticleButton from "./common/ArticleButton";
 import BlogCard from "./BlogCard";
-
+import { blogPosts } from "./data/blogPosts";
 
 
 export function SelectDemo() {
@@ -33,13 +33,19 @@ export function SelectDemo() {
 }
 
 export function InputDemo() {
-  return <Input type="email" placeholder="Search" className="w-[366px] bg-[#FFFFFF]"/>;
+  return (
+    <Input
+      type="email"
+      placeholder="Search"
+      className="w-[366px] bg-[#FFFFFF]"
+    />
+  );
 }
 
 function ArticleSection() {
   return (
     <>
-    {/* Mobile */}
+      {/* Mobile */}
       <div className="lg:hidden ">
         <p className="font-bold text-[24px] py-4 px-8">Last articles</p>
         <div className="pl-8 pr-8 mx-auto flex justify-center">
@@ -51,25 +57,39 @@ function ArticleSection() {
         </div>
       </div>
 
-    {/* Desktop */}
-        <p className="container mx-auto px-4 py-8 max-lg:hidden text-[24px] font-bold">Latest articles</p>
+      {/* Desktop */}
+      <p className="container mx-auto px-4 py-8 max-lg:hidden text-[24px] font-bold">
+        Latest articles
+      </p>
       <div className=" max-lg:hidden bg-[#EFEEEB] container px-4 py-4  mx-auto rounded-[16px] flex flex-row justify-between ">
         <div className="px-4">
-        <button className="w-[113px] h-[48px] bg-[#DAD6D1]">Highlight</button>
-            <ArticleButton text="Cat" />
-            <ArticleButton text="inspiration" />
-            <ArticleButton text="General" />
-            
-        </div >
-        <div className="px-4 py-2">
-        <InputDemo />
+          <button className="w-[113px] h-[48px] bg-[#DAD6D1]">Highlight</button>
+          <ArticleButton text="Cat" />
+          <ArticleButton text="inspiration" />
+          <ArticleButton text="General" />
         </div>
-
-
-
-        
+        <div className="px-4 py-2">
+          <InputDemo />
+        </div>
       </div>
-        <div className="lg:grid grid-cols-2 mx-auto py-8  container ">
+
+
+      <div className="lg:grid grid-cols-2 mx-auto py-8 gap-8 container">
+      {blogPosts.map((post) => (
+        
+        <BlogCard 
+          key={post.id}
+          category={post.category}
+          date={post.date}
+          topic={post.topic}
+          description={post.description}
+          image={post.image}
+        />
+        
+      ))}
+      </div>
+
+      {/*   <div className="lg:grid grid-cols-2 mx-auto py-8 container ">
         <BlogCard image="https://res.cloudinary.com/dcbpjtd1r/image/upload/v1728449771/my-blog-post/e739huvlalbfz9eynysc.jpg" category="General"
         alt="The Art of Mindfulness: Finding Peace in a Busy World" title="The Art of Mindfulness: Finding Peace in a Busy World" description="Discover the transformative power of mindfulness and how it can help you navigate the challenges of modern life with greater ease and contentment." author="Thompson P." date="11 September 2024"/>
         <BlogCard image="https://res.cloudinary.com/dcbpjtd1r/image/upload/v1728449771/my-blog-post/gsutzgam24abrvgee9r4.jpg" category="Cat" alt="The Secret Language of Cats: Decoding Feline Communication" title="The Secret Language of Cats: Decoding Feline Communication" description="Unravel the mysteries of cat communication and learn how to better understand your feline friend's needs and desires." author="Thompson P." date="21 August 2024"/>
@@ -77,9 +97,7 @@ function ArticleSection() {
         <BlogCard image="https://res.cloudinary.com/dcbpjtd1r/image/upload/v1728449771/my-blog-post/e0haxst38li4g8i0vpsr.jpg" category="General" alt="The Future of Work: Adapting to a Digital-First Economy" title="The Future of Work: Adapting to a Digital-First Economy" description="Explore how technology is reshaping the workplace and learn skills to succeed in the evolving job market." author="Thompson P." date="23 May 2024"/>
         <BlogCard image="https://res.cloudinary.com/dcbpjtd1r/image/upload/v1728449771/my-blog-post/g8qpepvgnz6gioylyhrz.jpg" category="Inspiration" alt="" title="The Power of Habits: Small Changes, Big Results" description="Discover how small, consistent habits can lead to significant personal and professional growth over time." author="Thompson P." date="23 June 2024"/>
         <BlogCard image="https://res.cloudinary.com/dcbpjtd1r/image/upload/v1728449771/my-blog-post/koydfh6jpmzhtxvwein3.jpg" category="Cat" alt="The Future of Work: Adapting to a Digital-First Economy" title="Cat Nutrition: A Guide to Feeding Your Feline Friend" description="Learn about the nutritional needs of cats and how to provide a balanced diet for optimal health and longevity." author="Thompson P." date="21 July 2024"/>
-
-        </div>
-
+        </div> */}
     </>
   );
 }
